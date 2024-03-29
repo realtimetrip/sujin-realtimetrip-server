08f1dad6bbb943784840d7f188407912cd6e68e1
sujin11-kim <su091876@gmail.com>
2024-03-30 00:38:32 +0900

    first commit

---
+++ src/main/java/sujin/realtimetrip/RealtimetripApplication.java
@@ -0,0 +1,13 @@
+package sujin.realtimetrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimetripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimetripApplication.class, args);
+	}
+
+}
