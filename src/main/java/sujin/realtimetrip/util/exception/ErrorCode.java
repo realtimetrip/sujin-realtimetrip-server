cc6e5f5571500e952658cf99076323af58f0e844
sujin11-kim <su091876@gmail.com>
2024-03-30 20:12:13 +0900

    Feat 사용자 로그인 기능 구현

--- src/main/java/sujin/realtimetrip/util/exception/ErrorCode.java
+++ src/main/java/sujin/realtimetrip/util/exception/ErrorCode.java
@@ -12,7 +12,8 @@ public enum ErrorCode {
     AUTH_CODE_VERIFICATION_FAILED(HttpStatus.BAD_REQUEST, "1001", "인증 번호 검증에 실패하였습니다. 다시 시도해 주세요."),
 
     //user
-    USER_EMAIL_DUPLICATED(HttpStatus.CONFLICT, "2001", "이미 사용 중인 이메일입니다. 다른 이메일을 사용해 주세요.");
+    USER_EMAIL_DUPLICATED(HttpStatus.CONFLICT, "2001", "이미 사용 중인 이메일입니다. 다른 이메일을 사용해 주세요."),
+    INVALID_CREDENTIALS(HttpStatus.BAD_REQUEST, "400", "이메일 또는 비밀번호가 올바르지 않습니다");
 
 
     private final HttpStatus status;
