ccdc62a621dbfd59d909744c2faa3d2e19b2108c
sujin11-kim <su091876@gmail.com>
2024-03-30 19:58:54 +0900

    Feat: 회원가입 기능 구현
    회원가입 및 중복 이메일 에러 반환 기능 구현

--- src/main/java/sujin/realtimetrip/User/controller/UserController.java
+++ src/main/java/sujin/realtimetrip/User/controller/UserController.java
@@ -1,8 +1,23 @@
 package sujin.realtimetrip.User.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import sujin.realtimetrip.User.dto.SignUpDto;
+import sujin.realtimetrip.User.dto.UserDto;
+import sujin.realtimetrip.User.service.UserService;
+import sujin.realtimetrip.util.ApiResponse;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
 
+    // 회원가입
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<UserDto>> signUpUser(@Valid @RequestBody SignUpDto signUpDto) {
+        return ResponseEntity.ok().body(ApiResponse.success(userService.signUp(signUpDto)));
+    }
 }
--- src/main/java/sujin/realtimetrip/util/exception/ErrorCode.java
+++ src/main/java/sujin/realtimetrip/util/exception/ErrorCode.java
@@ -9,7 +9,11 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     //mail
-    AUTH_CODE_VERIFICATION_FAILED(HttpStatus.BAD_REQUEST, "1001", "인증 번호 검증에 실패하였습니다. 다시 시도해 주세요.");
+    AUTH_CODE_VERIFICATION_FAILED(HttpStatus.BAD_REQUEST, "1001", "인증 번호 검증에 실패하였습니다. 다시 시도해 주세요."),
+
+    //user
+    USER_EMAIL_DUPLICATED(HttpStatus.CONFLICT, "2001", "이미 사용 중인 이메일입니다. 다른 이메일을 사용해 주세요.");
+
 
     private final HttpStatus status;
     private final String code;
