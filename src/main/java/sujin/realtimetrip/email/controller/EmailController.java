588bab960e6dd0c728310b690991b61845b086c0
sujin11-kim <su091876@gmail.com>
2024-04-01 23:28:45 +0900

    Refactor: EmailController와 RedisEmailController 통합 및 중복 코드 제거

--- src/main/java/sujin/realtimetrip/email/controller/EmailController.java
+++ src/main/java/sujin/realtimetrip/email/controller/EmailController.java
@@ -12,25 +12,36 @@ import sujin.realtimetrip.global.response.ApiResponse;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/mail")
+@RequestMapping("/email")
 public class EmailController {
 
     private final EmailService emailService;
 
-    // 인증 번호 이메일 전송
+    // 인증 번호 이메일 전송 (DB에 인증 번호 저장)
     @PostMapping("/send-authCode")
-    public ResponseEntity<ApiResponse<String>> mailConfirm(@RequestBody @Valid EmailDto emailDto) throws MessagingException {
-
-        // 이메일 인증을 두 번한 경우 이전 인증번호 삭제
+    public ResponseEntity<ApiResponse<String>> mailSend(@RequestBody @Valid EmailDto emailDto) throws MessagingException {
+        // 이메일 인증을 두 번한 경우 이전 인증 번호 삭제
         emailService.deleteExistCode(emailDto.getEmail());
-
-        return ResponseEntity.ok().body(ApiResponse.success(emailService.sendEmailAuthCode(emailDto.getEmail())));
+        return ResponseEntity.ok().body(ApiResponse.success(emailService.mailSend(emailDto.getEmail())));
     }
 
-    // 이메일 인증 번호 검사
-    @PostMapping("/check-authCode")
-    public ResponseEntity<ApiResponse<Boolean>> checkAuthCode(@RequestBody @Valid EmailAuthDto emailAuthDto){
+    // 이메일 인증 번호 검증 (DB에 저장된 인증 번호 검증)
+    @PostMapping("/verify-authCode")
+    public ResponseEntity<ApiResponse<Boolean>> verifyAuthCode(@RequestBody @Valid EmailAuthDto emailAuthDto){
         return ResponseEntity.ok().body(ApiResponse.success(emailService.verifyAuthCode(emailAuthDto.getEmail(), emailAuthDto.getAuthCode())));
 
     }
+
+    // 인증 번호 이메일 전송 (레디스로 인증 번호 저장)
+    @PostMapping("/send-authCode/with-redis")
+    public ResponseEntity<ApiResponse<String>> redisMailSend(@RequestBody @Valid EmailDto emailDto) throws MessagingException {
+        return ResponseEntity.ok().body(ApiResponse.success(emailService.redisMailSend(emailDto.getEmail())));
+    }
+
+    // 이메일 인증 번호 검증 (레디스에 저장된 인증 번호 검증)
+    @PostMapping("/verify-authCode/with-redis")
+    public ResponseEntity<ApiResponse<Boolean>> redisVerifyAuthCode(@RequestBody @Valid EmailAuthDto emailAuthDto){
+        return ResponseEntity.ok().body(ApiResponse.success(emailService.redisVerifyAuthCode(emailAuthDto.getEmail(),emailAuthDto.getAuthCode())));
+    }
+
 }
--- src/main/java/sujin/realtimetrip/email/dto/EmailDto.java
+++ src/main/java/sujin/realtimetrip/email/dto/EmailDto.java
@@ -1,8 +1,12 @@
 package sujin.realtimetrip.email.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class EmailDto {
+    @Email
+    @NotEmpty(message = "이메일을 입력해 주세요")
     private String email;
 }
