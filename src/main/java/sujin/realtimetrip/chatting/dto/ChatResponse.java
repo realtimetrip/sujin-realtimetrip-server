e6ff4d61272f7313d0a284bd26c941d2affe4f5a
sujin11-kim <su091876@gmail.com>
2024-05-18 18:05:39 +0900

    Feat(ChatResponse.java): 채팅 반환형 dto 추가

---
+++ src/main/java/sujin/realtimetrip/chatting/dto/ChatResponse.java
@@ -0,0 +1,24 @@
+package sujin.realtimetrip.chatting.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ChatResponse {
+    private String roomId;
+    private Long userId;
+    private String nickName;
+    private String message;
+    private LocalDateTime timestamp;
+
+    public ChatResponse(ChatRequest chatRequest) {
+        this.roomId = chatRequest.getRoomId();
+        this.userId = chatRequest.getUserId();
+        this.message = chatRequest.getMessage();
+        this.nickName = chatRequest.getNickName();
+        this.timestamp = LocalDateTime.now();
+    }
+}
