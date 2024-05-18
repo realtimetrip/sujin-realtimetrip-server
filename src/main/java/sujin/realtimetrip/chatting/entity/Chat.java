419972a447b0da482c916ea42b5f8ab38fc1911e
sujin11-kim <su091876@gmail.com>
2024-05-18 18:22:17 +0900

    Feat: 채팅, 채팅방 entity 생성

---
+++ src/main/java/sujin/realtimetrip/chatting/entity/Chat.java
@@ -0,0 +1,39 @@
+package sujin.realtimetrip.chatting.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sujin.realtimetrip.chatting.enums.MessageType;
+import sujin.realtimetrip.user.entity.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "chat")
+public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_id")
+    private Long id;
+    @Column(name = "message_type")
+    private MessageType messageType;
+    private String message;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
